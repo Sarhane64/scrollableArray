c301c40ab57dcad6ce43062252b12e0562c18180
theo sarhane <theosarhane@MacBook-Pro-de-theo.local>
2024-12-03 14:01:30 +0100

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'tesarray';
+  boardData = [
+    {
+      name: 'boardstandard',
+      price: [
+        { multiple: 12, metric: 'hour', price: 30 },
+        { multiple: 1, metric: 'day', price: 40 },
+        { multiple: 2, metric: 'day', price: 50 },
+        { multiple: 3, metric: 'day', price: 60 },
+        { multiple: 4, metric: 'day', price: 70 },
+        { multiple: 5, metric: 'day', price: 80 },
+        { multiple: 6, metric: 'day', price: 90 },
+        { multiple: 1, metric: 'week', price: 100 },
+      ],
+    },
+    {
+      name: 'boardpremium',
+      price: [
+        { multiple: 12, metric: 'hour', price: 40 },
+        { multiple: 1, metric: 'day', price: 50 },
+        { multiple: 2, metric: 'day', price: 60 },
+        { multiple: 3, metric: 'day', price: 70 },
+        { multiple: 4, metric: 'day', price: 80 },
+        { multiple: 5, metric: 'day', price: 90 },
+        { multiple: 6, metric: 'day', price: 100 },
+        { multiple: 1, metric: 'week', price: 110 },
+      ],
+    },
+  ];
 }
